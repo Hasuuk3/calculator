3fccf35afd0ad18f435ed015d488c953a66d491a
Hasuuk3 <hasnain.khatri34@gmail.com>
2023-05-13 21:10:46 +0500

    a
    a

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var value=document.getElementById("display")
+
+function btnval(num){
+display.value +=num;
+
+}
+
+function fnlans(a){
+    var a =display.value
+    var a=eval(a)
+display.value =a
+}
+
+function clearr(){
+
+    display.value ="";
+}
